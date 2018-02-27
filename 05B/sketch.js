1cffd1b807f444dd8fd8e8e092132a9c6b8b8691
feliperuedah <35973262+feliperuedah@users.noreply.github.com>
2018-02-27 00:22:00 -0500

    Add files via upload

---
+++ 05B/sketch.js
@@ -0,0 +1,73 @@
+//Cantidad por defecto de bolas
+var cant=10;
+
+//Posiciones de las bolas
+var x=[];
+var y=[];
+
+//Tamaño bolas
+var tam;
+
+function setup() 
+{ 
+  createCanvas(400, 400);
+  
+  //Posición inicial elipses
+  for(var i=0; i<cant; i++)
+  {
+    x[i]=random(0, width);
+    y[i]=random(0, height);
+  }
+} 
+
+function draw() 
+{ 
+  background(255);
+  
+  //Muestra el estado en caso de presionar una tecla
+  if(keyIsPressed)
+  {
+    h=hour()+minute()/60+second()/3600;
+
+    //Cambia el color del fondo según la distancia al medio día
+    r=map(abs(h-12), 0, 12, 72, 0);
+    g=map(abs(h-12), 0, 24, 151, 0);
+    b=map(abs(h-12), 0, 24, 145, 0);
+  
+    background(r, g, b);
+    
+    //Dispersión de las bolas segúna distancia al medio día
+    d=map(abs(h-12), 0, 12, -15, 1);
+    
+    //Tamaño bolas según segundo
+    tam=map(second(), 0, 60, 10, 50);
+    
+    //Cambia el color y transparencia de las bolas según el segundo
+    noStroke();
+    c=map(second(), 0, 60, 0, 180);
+    t=map(second(), 0, 6, 170, 220);
+    fill(c, 0, 0, 200);
+    
+    //Tantas bolas como la hora actual
+    cant=hour();
+    
+    //Ángulo minutero
+    tM=map(minute(), 0, 60, 0, TWO_PI)-HALF_PI;
+    
+    //Dibuja las bolas y actualiza sus pocisiones
+    for(var i=0; i<cant; i++)
+    {
+      x[i]=constrain(x[i]+random(-d, d), 0, width);
+      y[i]=constrain(y[i]+random(-d, d), 0, height);
+      
+      ellipse(x[i], y[i], tam, tam);
+      
+      //Minutero en cada elipse
+      push();
+      translate(x[i], y[i]);
+      fill(255);
+      ellipse(tam/2*cos(tM), tam/2*sin(tM), tam/5, tam/5);
+      pop();
+    }  
+  }
+}
